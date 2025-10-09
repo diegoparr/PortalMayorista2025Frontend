9bd677dbf4e84449f84f05d628c5654b0b3fec76
SebasSystem <sebas.maigual.system@gmail.com>
2025-10-08 19:39:08 -0500

    fix: update file-loader options to set outputPath and publicPath for image assets

--- webpack.config.js
+++ webpack.config.js
@@ -87,7 +87,9 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          outputPath: 'img',
+          name: '[name].[ext]?[hash]',
+          publicPath: '/img/'
         }
       }
     ]
